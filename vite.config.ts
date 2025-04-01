3e4e7a05254df33941c052edac01ad9d8a7b7ff9
klauswong888 <kls.wong.888@gmail.com>
2025-04-01 21:12:15 +1100

    tailwind css and react auto import setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    AutoImport({
+      imports: [
+        'react', //auto import useState, useEffect
+        'react-router-dom',
+        {
+          'zustand': ['useStore'], // zustand
+        },
+        {
+          'react-redux': ['useSelector', 'useDispatch'],
+        },
+        {
+          'axios': [['default', 'axios']]
+        },
+        {
+          'clsx': [['default', 'clsx']] 
+        }
+      ],
+      dts: './auto-imports.d.ts', // auto genrate import hooks type
+    }),
+  ],
 })
